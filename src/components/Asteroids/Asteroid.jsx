11c1a22406ba280f959c5dcf4aa219da54e3c46c
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2021-04-15 00:31:24 +0300

    added some styles (not done)

--- src/components/Asteroids/Asteroid.jsx
+++ src/components/Asteroids/Asteroid.jsx
@@ -13,40 +13,42 @@ const Asteroid = ({ asterId, inLunar }) => {
   const asteroid = getAsterFullInfoById(asterId);
   return (
     <article className="asteroid">
-      <div>
-        <Link to={`/asteroid/${asterId}`}>
-          <h2>{asteroid.name}</h2>
-        </Link>
+      <div className="aster-ic-1">
+        <div className="aster-ic-2">
+          <Link to={`/asteroid/${asterId}`}>
+            <h2>{asteroid.name}</h2>
+          </Link>
 
-        <div>Дата ... {asteroid.date_short}</div>
+          <div>Дата ... {asteroid.date_short}</div>
 
-        <div>
-          Расстояние {"..."}{" "}
-          <span>
-            {inLunar
-              ? asteroid.distance_lunar + " луны"
-              : asteroid.distance + " км"}
-          </span>
-        </div>
+          <div>
+            Расстояние {"..."}{" "}
+            <span>
+              {inLunar
+                ? asteroid.distance_lunar + " луны"
+                : asteroid.distance + " км"}
+            </span>
+          </div>
 
-        <div>
-          Размер {"..."} {asteroid.size + " м"}
+          <div>
+            Размер {"..."} {asteroid.size + " м"}
+          </div>
         </div>
       </div>
 
-      <div>
+      <div className="aster-ic-1">
         <div>
-          Оценка:
-          {asteroid.isHazardous ? "Опасный" : "Неопасный"}
+          <div>Оценка:</div>
+          <div>{asteroid.isHazardous ? "Опасный" : "Неопасный"}</div>
+          <button
+            onClick={() => {
+              addAsterToDestroyList(asterId);
+              deleteAsterFromTheMainList(asterId);
+            }}
+          >
+            На уничтожение
+          </button>
         </div>
-        <button
-          onClick={() => {
-            addAsterToDestroyList(asterId);
-            deleteAsterFromTheMainList(asterId);
-          }}
-        >
-          На уничтожение
-        </button>
       </div>
     </article>
   );
