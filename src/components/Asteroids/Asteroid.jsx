9866f9aed5b066a087609abba2ef97d57be41074
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2021-04-15 19:01:54 +0300

    changed some styles

--- src/components/Asteroids/Asteroid.jsx
+++ src/components/Asteroids/Asteroid.jsx
@@ -1,3 +1,8 @@
+import small from "../../svg/small.svg";
+import middle from "../../svg/middle.svg";
+import big from "../../svg/big.svg";
+import dino from "../../svg/dino.svg";
+
 import "../../styles/StyleReseter.scss";
 import "./Asteroid.scss";
 
@@ -6,43 +11,100 @@ import { Link } from "react-router-dom";
 import { useAsteroidList } from "../../providers/AsteroidProvider";
 import { useDestroyService } from "../../providers/DestroyProvider";
 
+const setGradientBg = (isHazardous) => {
+  if (isHazardous) {
+    return "asteroid-bg-danger";
+  }
+  return "asteroid-bg-safe";
+};
+
+const setSvgToImg = (size) => {
+  let path = null;
+
+  if (size <= 85) {
+    path = small;
+    return { id: "small", path };
+  }
+  if (size > 85 && size <= 300) {
+    path = middle;
+    return { id: "middle", path };
+  }
+  if (size > 300) {
+    path = big;
+    return { id: "big", path };
+  }
+};
+
 const Asteroid = ({ asterId, inLunar }) => {
   const { addAsterToDestroyList } = useDestroyService();
   const {
     deleteAsterFromTheMainList,
     getAsterFullInfoById,
   } = useAsteroidList();
-
   const asteroid = getAsterFullInfoById(asterId);
+  const svg = setSvgToImg(asteroid.size);
+
   return (
-    <article className="asteroid">
-      <div className="aster-ic-1">
-        <div className="aster-ic-2">
-          <Link to={`/asteroid/${asterId}`}>
-            <h2>{asteroid.name}</h2>
-          </Link>
+    <article className={`asteroid ${setGradientBg(asteroid.isHazardous)} `}>
+      <img
+        src={svg.path}
+        className={`asteroid-img aster-${svg.id}`}
+        alt={svg.id}
+      />
+      <img className="dino-img" src={dino} alt="dino" />
+      <div className="asteroid-inner-1">
+        <div className="asteroid-inner-1-container-1">
+          <h2 className="asteroid-inner-1-container-1-title">
+            <Link
+              className="asteroid-inner-1-container-1-title-link"
+              to={`/asteroid/${asterId}`}
+            >
+              {asteroid.name}
+            </Link>
+          </h2>
 
-          <div>Дата ... {asteroid.date_short}</div>
-
-          <div>
-            Расстояние {"..."}{" "}
-            <span>
-              {inLunar
-                ? asteroid.distance_lunar + " луны"
-                : asteroid.distance + " км"}
-            </span>
+          <div className="asteroid-inner-1-container-1-item">
+            <div className="asteroid-inner-1-container-1-item-1">
+              <span>Дата</span>
+            </div>
+            <div className="asteroid-inner-1-container-1-item-2"></div>
+            <div className="asteroid-inner-1-container-1-item-3">
+              {asteroid.date_short}
+            </div>
           </div>
 
-          <div>
-            Размер {"..."} {asteroid.size + " м"}
+          <div className="asteroid-inner-1-container-1-item">
+            <div>
+              <span> Расстояние</span>
+            </div>
+            <div className="asteroid-inner-1-container-1-item-2"> </div>
+            <div>
+              <span>
+                {inLunar
+                  ? asteroid.distance_lunar + " луны"
+                  : asteroid.distance + " км"}
+              </span>
+            </div>
+          </div>
+
+          <div className="asteroid-inner-1-container-1-item">
+            <div>
+              <span> Размер </span>
+            </div>
+            <div className="asteroid-inner-1-container-1-item-2"></div>
+            <div>
+              <span>{asteroid.size + " м"}</span>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="aster-ic-1">
-        <div>
+      <div className="asteroid-inner-2">
+        <div className="asteroid-inner-2-container-1">
           <div>Оценка:</div>
-          <div>{asteroid.isHazardous ? "Опасный" : "Неопасный"}</div>
+          <div className="asteroid-inner-2-container-1-isdanger">
+            <span>{asteroid.isHazardous ? "опасен" : "неопасен"}</span>
+          </div>
           <button
             onClick={() => {
               addAsterToDestroyList(asterId);
